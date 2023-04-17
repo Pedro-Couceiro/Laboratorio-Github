65831c42821c7e4211f18442c2ba74d42a0da584
joaocorreia <jphenriquescorreia@gmail.com>
2023-04-17 12:01:45 +0100

    enemy movement script edited

--- Laboratorio/Assets/Scripts/MovingEnemy.cs
+++ Laboratorio/Assets/Scripts/MovingEnemy.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class MovingEnemy : MonoBehaviour
 {
     [SerializeField] private float Range;
+    private GameObject player;
+    private float location;
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Teste");
+        if (collision.CompareTag("Player"))
+        {
+            Vector2 direction = (player.transform.position - transform.position).normalized;
+        }
+            
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
